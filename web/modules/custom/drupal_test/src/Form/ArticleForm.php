6b03b20011808da2f73d90af1572cb81379cdd63
Volodya <isaiev_v_o@ukr.net>
2021-10-07 23:15:43 +0300

    Initial commit

---
+++ web/modules/custom/drupal_test/src/Form/ArticleForm.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\drupal_test\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\Query\QueryFactoryInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ArticleForm extends FormBase
+{
+
+  protected $config;
+  protected $entityType;
+
+  public function __construct(ConfigFactoryInterface $configFactory,  EntityTypeManagerInterface $entityType) {
+    $this->config = $configFactory;
+    $this->entityType = $entityType;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  public function getFormId()
+  {
+    return 'articles_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $articles = $this->entityType->getStorage('node')->loadByProperties(['type' => 'article']);
+    $titles = [];
+    foreach ($articles as $article) {
+      $titles[$article->id()] = $article->label();
+    }
+
+    $form['title'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Article title'),
+      '#options' => $titles,
+    ];
+
+    $form['status'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Article status'),
+      '#options' => ['published' => 'published', 'unpublished' => 'unpublished'],
+    ];
+
+    $form['sticky'] = [
+      '#type' => 'select',
+      '#title' => $this->t('sticky'),
+      '#options' => ['sticky' => 'sticky', 'not sticky' => 'not sticky'],
+    ];
+
+    $form['actions']['#type'] = 'actions';
+
+    $form['actions']['update'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Update'),
+      '#submit' => [$this, 'update'],
+    ];
+
+    $form['actions']['delete'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Delete'),
+      '#submit' => [$this, 'delete'],
+    ];
+
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    return;
+  }
+
+}
---
+++ web/modules/custom/drupal_test/src/Form/TestForm.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\drupal_test\Form;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class TestForm extends FormBase
+{
+
+  protected $config;
+
+  public function __construct(ConfigFactoryInterface $configFactory) {
+    $this->config = $configFactory;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+    );
+  }
+
+  public function getFormId()
+{
+  return 'test_form';
+}
+
+  public function buildForm(array $form, FormStateInterface $form_state)
+{
+
+  $form['text'] = [
+    '#type' => 'textfield',
+    '#title' => $this->t('Title'),
+    '#placeholder' => $this->config('system.site')->get('name'),
+  ];
+  $form['actions']['#type'] = 'actions';
+  $form['actions']['submit'] = [
+    '#type' => 'submit',
+    '#value' => $this->t('Submit'),
+    '#button_type' => 'primary',
+  ];
+  return $form;
+}
+
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (strlen($form_state->getValue('text')) < 6) {
+      $form_state->setErrorByName('text', $this->t('The site name is too short. Please enter at least 6 chars.'));
+    }
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state)
+{
+  if($form_state->getValue('text')){
+    $this->config->getEditable('system.site')->set('name',$form_state->getValue('text'))->save();
+  }
+}
+}
